b791cdcd11959e68f528b3d93f788dd10cd39dd6
Madeline Cowie <madeline@tidelift.com>
2022-11-07 17:38:43 -0600

    Linter happiness

--- src/main.ts
+++ src/main.ts
@@ -1,143 +1,160 @@
-import * as core from '@actions/core'
+import {getInput, setFailed} from '@actions/core'
 import * as github from '@actions/github'
-import { Context } from "@actions/github/lib/context"
+import {Context} from '@actions/github/lib/context'
 
-import { TideliftRecommendation, getTideliftRecommendations } from "./tidelift_recommendation"
+import {
+  TideliftRecommendation,
+  getTideliftRecommendations
+} from './tidelift_recommendation'
 
-const myToken = core.getInput("repo-token");
-const octokit = github.getOctokit(myToken);
+const myToken = getInput('repo-token')
+const octokit = github.getOctokit(myToken)
 
 export type VulnerabilityId = string
 export type IssueNumber = number
 export type RepoOwner = string
 export type RepoName = string
 
-async function getIssue(repoOwner: RepoOwner, repoName: RepoName, issueNumber: IssueNumber) {
+async function getIssue(
+  repoOwner: RepoOwner,
+  repoName: RepoName,
+  issueNumber: IssueNumber
+): Promise<object> {
   return octokit.rest.issues.get({
     owner: repoOwner,
     repo: repoName,
     issue_number: issueNumber
-  });
+  })
 }
 
-async function addLabels(repoOwner: RepoOwner, repoName: RepoName, issueNumber: IssueNumber, labels: string[]) {
-  return octokit.rest.issues.addLabels({
-    owner: repoOwner,
-    repo: repoName,
-    issue_number: issueNumber,
-    labels: labels
-  });
+async function addLabels(
+  owner: RepoOwner,
+  repo: RepoName,
+  issue_number: IssueNumber,
+  labels: string[]
+): Promise<object> {
+  return octokit.rest.issues.addLabels({owner, repo, issue_number, labels})
 }
 
-async function addComment(repoOwner: RepoOwner, repoName: RepoName, issueNumber: IssueNumber, body: string) {
+async function addComment(
+  owner: RepoOwner,
+  repo: RepoName,
+  issue_number: IssueNumber,
+  body: string
+): Promise<object> {
   return octokit.rest.issues.createComment({
-    owner: repoOwner,
-    repo: repoName,
-    issue_number: issueNumber,
-    body: body
-  });
+    owner,
+    repo,
+    issue_number,
+    body
+  })
 }
 
 function issueHasBeenAssigned(issue): boolean {
   return issue.data.assignees.length !== 0
 }
 
-function getIssueNumber(core, context: Context): IssueNumber {
-  let issueNumber = core.getInput("issue-number");
+function getIssueNumber(context: Context): IssueNumber | undefined {
+  const possibleNumber =
+    getInput('issue-number') ||
+    context.payload?.issue?.number ||
+    context.payload?.pull_request?.number
 
-  // return what is provided
-  if (issueNumber) return issueNumber;
-
-  // return the one found in issue
-  issueNumber = context.payload.issue && context.payload.issue.number;
-  if (issueNumber) return issueNumber;
-
-  // return the one found in PR
-  issueNumber =
-    context.payload.pull_request && context.payload.pull_request.number;
-  if (issueNumber) return issueNumber;
-
-  let card_url =
-    context.payload.project_card && context.payload.project_card.content_url;
-  issueNumber = card_url && card_url.split("/").pop();
-
-  return issueNumber;
+  if (possibleNumber) return Number(possibleNumber)
 }
 
 function findMentionedCves(issue): VulnerabilityId[] {
-  const regex = /CVE-[\d]+-[\d]+/ig
-  const searchFields = ["body", "title"]
+  const regex = /CVE-[\d]+-[\d]+/gi
+  const searchFields = ['body', 'title']
 
-  return Array.from(new Set(searchFields.
-    map(field => issue.data[field]).
-    filter(field => typeof(field) === 'string').
-    flatMap(field => field.match(regex)).
-    filter(field => field)
-  ))
+  return Array.from(
+    new Set(
+      searchFields
+        .map(field => issue.data[field])
+        .filter(field => typeof field === 'string')
+        .flatMap(field => field.match(regex))
+        .filter(field => field)
+    )
+  )
 }
 
 function formatLabelName(cve: VulnerabilityId): string {
-  return`:yellow_circle: ${cve.toUpperCase()}`
+  return `:yellow_circle: ${cve.toUpperCase()}`
 }
 
-function formatRecommendationText(recommendation: TideliftRecommendation): string {
-  return`:wave: Looks like you're reporting ${recommendation.vuln_id}.\n\n${JSON.stringify(recommendation)}`
+function formatRecommendationText(
+  recommendation: TideliftRecommendation
+): string {
+  return `:wave: Looks like you're reporting ${
+    recommendation.vuln_id
+  }.\n\n${JSON.stringify(recommendation)}`
 }
 
 function formatRecommendationsComment(recs: TideliftRecommendation[]): string {
-  return recs.map(formatRecommendationText).join("\n\n")
+  return recs.map(formatRecommendationText).join('\n\n')
 }
 
-async function scanIssue() {
+async function scanIssue(): Promise<string> {
   const repoName = github.context?.payload?.repository?.name as RepoName
-  const repoOwner = github.context?.payload?.repository?.owner?.login as RepoOwner
-  var ignoreIfAssigned = false
+  const repoOwner = github.context?.payload?.repository?.owner
+    ?.login as RepoOwner
+  // eslint-disable-next-line prefer-const
+  let ignoreIfAssigned = false
 
-  let issueNumber = getIssueNumber(core, github.context)
+  const issueNumber = getIssueNumber(github.context)
 
   if (issueNumber === undefined) {
-    return "No action being taken. Ignoring because issueNumber was not identified";
+    return 'No action being taken. Ignoring because issueNumber was not identified'
   }
 
   // Refresh for latest changes
-  var issue = await getIssue(repoOwner, repoName, issueNumber)
+  // eslint-disable-next-line prefer-const
+  let issue = await getIssue(repoOwner, repoName, issueNumber)
 
   if (ignoreIfAssigned && issueHasBeenAssigned(issue)) {
-    return "No action being taken. Ignoring because one or more assignees have been added to the issue";
+    return 'No action being taken. Ignoring because one or more assignees have been added to the issue'
   }
 
-  let mentionedCves = findMentionedCves(issue)
+  const mentionedCves = findMentionedCves(issue)
 
-  console.log("CVES found:", mentionedCves)
-
-  if (mentionedCves.length == 0) {
-    return "Did not find any CVEs mentioned";
+  if (mentionedCves.length === 0) {
+    return 'Did not find any CVEs mentioned'
   }
 
-  await addLabels(repoOwner, repoName, issueNumber, mentionedCves.map(formatLabelName))
+  await addLabels(
+    repoOwner,
+    repoName,
+    issueNumber,
+    mentionedCves.map(formatLabelName)
+  )
 
-  let recs = await getTideliftRecommendations(mentionedCves)
+  const recs = await getTideliftRecommendations(mentionedCves)
 
-  if (recs.length == 0) {
-    return `Did not find any Tidelift recommendations for CVEs: ${mentionedCves}`;
+  if (recs.length === 0) {
+    return `Did not find any Tidelift recommendations for CVEs: ${mentionedCves}`
   }
 
-  await addComment(repoOwner, repoName, issueNumber, formatRecommendationsComment(recs))
-  
+  await addComment(
+    repoOwner,
+    repoName,
+    issueNumber,
+    formatRecommendationsComment(recs)
+  )
+
   return `Found: ${mentionedCves}; Recs: ${recs}`
 }
 
-
 async function run(): Promise<void> {
   try {
-    scanIssue().
-      then(result => {
-        console.log(result)
-      }).catch(err => {
-        core.setFailed(err)
-      })
+    await scanIssue()
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    let msg
+    if (error instanceof Error) {
+      msg = error.message
+    } else {
+      msg = error
+    }
+    setFailed(msg)
   }
 }
 
--- src/tidelift_recommendation.ts
+++ src/tidelift_recommendation.ts
@@ -1,14 +1,12 @@
-import { VulnerabilityId } from "./main"
-import { getInput } from "@actions/core"
+import {VulnerabilityId} from './main'
+import {getInput} from '@actions/core'
 import axios from 'axios'
-
-const tideliftToken = getInput("tidelift-token")
 
 export class TideliftRecommendation {
   vuln_id: VulnerabilityId
   description: string
   severity: number
-  recommendation_created_at: Date 
+  recommendation_created_at: Date
   recommendation_updated_at: Date
   impact_score: number
   impact_description: string
@@ -23,45 +21,59 @@ export class TideliftRecommendation {
 
   constructor(vuln_id: VulnerabilityId, recommendationData: Object) {
     this.vuln_id = vuln_id
-    this.description = recommendationData["description"]
-    this.severity = recommendationData["severity"]
-    this.recommendation_created_at = recommendationData["recommendation_created_at"]
-    this.recommendation_updated_at = recommendationData["recommendation_updated_at"]
-    this.impact_score = recommendationData["impact_score"]
-    this.impact_description = recommendationData["impact_description"]
-    this.other_conditions = recommendationData["other_conditions"]
-    this.other_conditions_description = recommendationData["other_conditions_description"]
-    this.workaround_available = recommendationData["workaround_available"]
-    this.workaround_description = recommendationData["workaround_description"]
-    this.specific_methods_affected = recommendationData["specific_methods_affected"]
-    this.specific_methods_description = recommendationData["specific_methods_description"]
-    this.real_issue = recommendationData["real_issue"]
-    this.false_positive_reason = recommendationData["false_positive_reason"]   
+    this.description = recommendationData['description']
+    this.severity = recommendationData['severity']
+    this.recommendation_created_at =
+      recommendationData['recommendation_created_at']
+    this.recommendation_updated_at =
+      recommendationData['recommendation_updated_at']
+    this.impact_score = recommendationData['impact_score']
+    this.impact_description = recommendationData['impact_description']
+    this.other_conditions = recommendationData['other_conditions']
+    this.other_conditions_description =
+      recommendationData['other_conditions_description']
+    this.workaround_available = recommendationData['workaround_available']
+    this.workaround_description = recommendationData['workaround_description']
+    this.specific_methods_affected =
+      recommendationData['specific_methods_affected']
+    this.specific_methods_description =
+      recommendationData['specific_methods_description']
+    this.real_issue = recommendationData['real_issue']
+    this.false_positive_reason = recommendationData['false_positive_reason']
   }
 }
 
-export async function getTideliftRecommendation(vuln_id: VulnerabilityId): Promise<TideliftRecommendation> {
-
-  let config = {
+export async function getTideliftRecommendation(
+  vuln_id: VulnerabilityId
+): Promise<TideliftRecommendation | undefined> {
+  const config = {
     headers: {
-      'Authorization': `Bearer ${tideliftToken}`
+      Authorization: `Bearer ${tideliftToken}`
     }
   }
+  try {
+    const response = await axios.get(
+      `https://api.tidelift.com/external-api/v1/vulnerability/${vuln_id}/recommendation`,
+      config
+    )
 
-  return axios.get(`https://api.tidelift.com/external-api/v1/vulnerability/${vuln_id}/recommendation`, config).
-    then(response => response.data).
-    catch(err => {
-      if (err.response && err.response.status === 404) {
-        return null
-      }
-
-      console.error(`Problem fetching Tidelift Recommendations for: ${vuln_id}`)
-    }).
-    then(data => new TideliftRecommendation(vuln_id, data))
+    return new TideliftRecommendation(vuln_id, response.data)
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      // Not Found
+    }
+    // eslint-disable-next-line no-console
+    console.error(`Problem fetching Tidelift Recommendations for: ${vuln_id}`)
+  }
 }
 
-
-export async function getTideliftRecommendations(vuln_ids: VulnerabilityId[]): Promise<TideliftRecommendation[]> {
-  let recs = await Promise.all(vuln_ids.map(vuln_id => getTideliftRecommendation(vuln_id)))
-  return recs.filter(r => r)
+export async function getTideliftRecommendations(
+  vuln_ids: VulnerabilityId[]
+): Promise<TideliftRecommendation[]> {
+  const recs = await Promise.all(vuln_ids.map(getTideliftRecommendation))
+  return recs.filter(
+    r => r instanceof TideliftRecommendation
+  ) as TideliftRecommendation[]
 }
+
+const tideliftToken = getInput('tidelift-token')
